f532b523845ec1d8b89ed825e477a3e3126a019e
yamazaki <wadashiwa666@sina.com>
2019-01-13 09:10:10 +0800

    gen

---
+++ src/com/vo/UserRole.java
@@ -0,0 +1,45 @@
+package com.vo;
+
+import java.util.Date;
+
+public class UserRole {
+    private Integer id;
+
+    private Integer userId;
+
+    private Integer roleId;
+
+    private Date createDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
